641e499884fed8589183d26899009e511d759016
yntethan <49999336+yntethan@users.noreply.github.com>
2022-10-27 21:02:42 -0500

    Update handshake.cpp

--- handshake.cpp
+++ handshake.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <thread>
+#include <string>
+#include <condition_variable>
+#include <mutex>
 // include additional necessary headers
+using namespace std;
 
-void query(/*add necessary parameters*/) {
+mutex m;
+condition_variable q;
+condition_variable r;
+int n = 0;
+
+void query(/*add necessary parameters*/ int c) {
     // Should print: the print number (starting from 0), "SYN", and the three dots "..."
+    for (; n < c; n++) {
+        unique_lock<mutex> lock(m);
+        q.wait(lock, [&]{return n % 2 == 0;});
+        cout << "[" << n/2 << "] SYN ... ";
+        cout.flush();
+        lock.unlock();
+        r.notify_one();
+    }
 }
 
-void response(/*add necessary parameters*/) {
+void response(/*add necessary parameters*/ int c) {
     // Should print "ACK"
+    for (; n <= c; n++) {
+        unique_lock<mutex> lock(m);
+        r.wait(lock, [&]{return n % 2 == 1;});
+        cout << "ACK";
+        cout.flush();
+        lock.unlock();
+        q.notify_one();
+    }
 }
 
 int main(int argc, char** argv) {
@@ -14,7 +40,6 @@ int main(int argc, char** argv) {
         printf("Usage: ./handshake <count>\n");
         exit(1);
     }
-
     /**
      * Steps to follow:
      * 1. Get the "count" from cmd args
@@ -23,6 +48,12 @@ int main(int argc, char** argv) {
      * 4. Provide the threads with necessary args
      * 5. Update the "query" and "response" functions to synchronize the output
     */
+    int count = stoi(argv[1]) * 2;
+
+    thread qthread (query, count),
+           rthread (response, count);
+    qthread.join();
+    rthread.join();
    
     return 0;
 }
